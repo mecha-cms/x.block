7db60c6143117ec147cfc27c2a172f45467b73f1
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-01-08 18:09:32 +0700

    Allow Broken Blocks in Container Blocks
    https://github.com/mecha-cms/extend.block/issues/1

--- block/engine/kernel/block.php
+++ block/engine/kernel/block.php
@@ -25,20 +25,19 @@ class Block extends Genome {
         return true;
     }
 
-    public static function replace($id, $fn, $content, $strict = false) {
+    public static function replace($id, $fn, $content) {
         $state = Extend::state(Path::D(__DIR__, 2));
         $union = new Union($state['union']);
         $d = '#';
         $u = $state['union'][1];
         $ueo = $u[0][0]; // `[[`
         $uec = $u[0][1]; // `]]`
-        $uex = $u[0][2]; // `/`
+        $uee = $u[0][2]; // `/`
         $uas = $u[1][3]; // ` `
         $ueo_x = x($u[0][0], $d);
         $uec_x = x($u[0][1], $d);
-        $uex_x = x($u[0][2], $d);
+        $uee_x = x($u[0][2], $d);
         $uas_x = x($u[1][3], $d);
-        $a_x = x(Anemon::NS, $d);
         $id_x = x($id, $d);
         // quick replace with static output …
         if (!is_callable($fn)) {
@@ -50,19 +49,19 @@ class Block extends Genome {
         if (strpos($content, $ueo) === false) {
             return $content;
         }
-        // no `[[id]]` and `[[id/]]` character(s) found, skip …
+        // no `[[id]]`, `[[id/]]` and `[[id /]]` character(s) found, skip …
         if (
             strpos($content, $ueo . $id . $uec) === false &&
-            strpos($content, $ueo . $id . $uex . $uec) === false
+            strpos($content, $ueo . $id . $uee . $uec) === false &&
+            strpos($content, $ueo . $id . $uas . $uee . $uec) === false
         ) {
             return $content;
         }
+        // prioritize container block(s) over void block(s)
         // check for `[[/` character(s) …
-        if (strpos($content, $ueo . $uex) !== false) {
-            $id_e = explode(Anemon::NS, $id)[0];
-            $id_e_x = $strict ? $id_x : x($id_e, $d) . '(?:' . $a_x . '[^' . $a_x . $uas_x . ']+)*';
+        if (strpos($content, $ueo . $uee) !== false) {
             // `[[id]]content[[/id]]`
-            $s = $ueo_x . $id_x . '(' . $uas_x . '.*?)?' . $uec_x . '[\s\S]*?' . $ueo_x . $uex_x . $id_e_x . $uec_x;
+            $s = $ueo_x . $id_x . '(?:' . $uas_x . '*' . $uee_x . $uec_x . '|' . $uec_x . '(?:(?!' . $ueo_x . $uee_x . $id_x . $uec_x . ')[\s\S]*?' . $ueo_x . $uee_x . $id_x . $uec_x . ')?)';
             $content = preg_replace_callback($d . $s . $d, function($m) use($union, $fn) {
                 $data = $union->apart(array_shift($m));
                 array_shift($data); // remove “node name” data
@@ -76,7 +75,7 @@ class Block extends Genome {
             // have some void block(s) with attribute(s) in it …
             if (strpos($content, $ueo . $id . $uas) !== false) {
                 // `[[id foo="bar"]]` or `[[id foo="bar"/]]`
-                $s = $ueo_x . $id . '(' . $uas_x . '.*?)?' . $uex_x . $uec_x;
+                $s = $ueo_x . $id_x . '(' . $uas_x . '.*?)?' . $uas_x . '*' . $uee_x . $uec_x;
                 $content = preg_replace_callback($d . $s . $d, function($m) use($union, $fn) {
                     $data = $union->apart(array_shift($m));
                     array_shift($data); // remove “node name” data
@@ -85,10 +84,11 @@ class Block extends Genome {
             // else, void block(s) with no attribute(s)
             // we can replace them as quick as possible …
             } else {
-                // `[[id]]` or `[[id/]]`
+                // `[[id]]`, `[[id/]]` and `[[id /]]`
                 $content = str_replace([
                     $ueo . $id . $uec,
-                    $ueo . $id . $uex . $uec
+                    $ueo . $id . $uee . $uec,
+                    $ueo . $id . $uas . $uee . $uec
                 ], call_user_func_array($fn, [false, [], []]), $content);
             }
         }
