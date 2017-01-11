ff3f5397ad7b81b7d1548e5f667fa569823e922d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-01-12 00:51:02 +0700

    Update Hooks

--- block/index.php
+++ block/index.php
@@ -8,34 +8,25 @@ Lot::set([
     'ui' => uniqid()
 ], __DIR__);
 
-function fn_block_x($data) {
+function fn_block_x($content) {
     extract(Lot::get(null, [], __DIR__));
-    if (empty($data['content'])) {
-        return $data;
-    }
-    $content = $data['content'];
     if (strpos($content, $ux[0]) === false) {
-        return $data;
+        return $content;
     }
-    $data['content'] = str_replace([$ux[0], $ux[1]], [X . $ui, $ui . X], $content);
-    return $data;
+    return str_replace([$ux[0], $ux[1]], [X . $ui, $ui . X], $content);
 }
 
-function fn_block($data) {
+function fn_block($content) {
     extract(Lot::get(null, [], __DIR__));
-    if (!empty($data['content'])) {
-        $content = $data['content'];
-        // no `[[` character(s) found, skip anyway …
-        if (strpos($content, $ue[0]) === false && strpos($content, X . $ui) === false) {
-            return $data;
-        }
-        foreach (Block::get(null, []) as $k => $v) {
-            $content = call_user_func($v, $content);
-        }
-        $data['content'] = str_replace([X . $ui, $ui . X], [$ue[0], $ue[1]], $content);
+    // no `[[` character(s) found, skip anyway …
+    if (strpos($content, $ue[0]) === false && strpos($content, X . $ui) === false) {
+        return $content;
     }
-    return $data;
+    foreach (Block::get(null, []) as $k => $v) {
+        $content = call_user_func($v, $content);
+    }
+    return str_replace([X . $ui, $ui . X], [$ue[0], $ue[1]], $content);
 }
 
-Hook::set('page.input', 'fn_block_x', 0);
-Hook::set('page.input', 'fn_block', 1);
+Hook::set('page.content', 'fn_block_x', 0);
+Hook::set('page.content', 'fn_block', 1);
