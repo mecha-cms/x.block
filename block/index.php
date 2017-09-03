356870371dc4bc79c068f2ad157e662e6a526b16
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-09-03 20:05:01 +0700

    Add Instance and Key Parameter

--- block/index.php
+++ block/index.php
@@ -4,7 +4,7 @@ if ($state = File::exist(__DIR__ . DS . 'lot' . DS . 'state' . DS . 'config.php'
     Block::$config = array_replace_recursive(Block::$config, require $state);
 }
 
-function fn_block_x($content, $lot = []) {
+function fn_block_x($content, $lot = [], $that = null, $key = null) {
     $state = Block::$config['union'];
     $ue = $state[1][0];
     $ux = $state[1][3];
@@ -15,7 +15,7 @@ function fn_block_x($content, $lot = []) {
     return str_replace([$ux[0], $ux[1]], [X . $ui, $ui . X], $content);
 }
 
-function fn_block($content, $lot = []) {
+function fn_block($content, $lot = [], $that = null, $key = null) {
     $state = Block::$config['union'];
     $ue = $state[1][0];
     $ux = $state[1][3];
@@ -25,21 +25,21 @@ function fn_block($content, $lot = []) {
         return $content;
     }
     foreach (Anemon::eat(Block::get(null, []))->sort([1, 'stack'])->vomit() as $k => $v) {
-        $content = call_user_func($v['fn'], $content, $lot);
+        $content = call_user_func($v['fn'], $content, $lot, $that, $key);
     }
     return str_replace([X . $ui, $ui . X], [$ue[0], $ue[1]], $content);
 }
 
 Hook::set([
-    'page.content',
-    'page.css',
-    'page.description',
-    'page.js'
+    '*.content',
+    '*.css',
+    '*.description',
+    '*.js'
 ], 'fn_block_x', 0);
 
 Hook::set([
-    'page.content',
-    'page.css',
-    'page.description',
-    'page.js'
+    '*.content',
+    '*.css',
+    '*.description',
+    '*.js'
 ], 'fn_block', 1);
