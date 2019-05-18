0b60968a4e8b99cf8544d0e747064ab8b9a59db5
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-05-18 12:47:59 +0700

    Remove Candy

--- block/index.php
+++ block/index.php
@@ -11,14 +11,18 @@ function block($content) {
         return $content;
     }
     foreach (\g(BLOCK, 'data') as $v) {
-        $content = \Block::replace($k = \Path::N($v), function($a, $b) use($k, $v) {
+        $content = \Block::replace($k = \Path::N($v), function($a, $b) use($k) {
             $data = [
                 0 => $k,
                 1 => $a,
                 2 => $b
             ];
             $data[2] = \json_encode($data[2]);
-            return \candy(\file_get_contents($v), \extend($data, $b));
+            $content = \file_get_contents($v);
+            foreach (\array_replace($data, $b) as $k => $v) {
+                $content = \str_replace('%' . $k, $v, $content);
+            }
+            return $content;
         }, $content);
     }
     foreach (\Anemon::eat(\Block::get())->sort([1, 'stack'], true) as $k => $v) {
