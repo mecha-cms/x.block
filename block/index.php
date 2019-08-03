f4c40693de597f0a7da56e37330743ab77f6c0ea
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-08-03 21:35:30 +0700

    Update index.php

--- block/index.php
+++ block/index.php
@@ -1,4 +1,4 @@
-<?php namespace _;
+<?php namespace _\lot\x;
 
 if ($state = \state('block')) {
     \Block::$config = \extend(\Block::$config, (array) $state);
@@ -10,15 +10,15 @@ function block($content) {
     if (\strpos($content, $c[0][0]) === false) {
         return $content;
     }
-    foreach (\g(BLOCK, 'data') as $v) {
-        $content = \Block::replace($k = \Path::N($v), function($a, $b) use($k) {
+    foreach (\g(BLOCK, 'data') as $k => $v) {
+        $content = \Block::replace($n = \Path::N($k), function($a, $b) use($n, $k) {
             $data = [
-                0 => $k,
+                0 => $n,
                 1 => $a,
                 2 => $b
             ];
             $data[2] = \json_encode($data[2]);
-            $content = \file_get_contents($v);
+            $content = \file_get_contents($k);
             foreach (\array_replace($data, $b) as $k => $v) {
                 $content = \str_replace('%' . $k, $v, $content);
             }
